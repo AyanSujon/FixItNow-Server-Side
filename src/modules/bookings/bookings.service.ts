0a33622fb08735449934208cd0481aa74749ac80
Ayan <sujon.seo.bd@gmail.com>
2026-07-08 22:21:28 +0600

    updated code

--- src/modules/bookings/bookings.service.ts
+++ src/modules/bookings/bookings.service.ts
@@ -5,6 +5,87 @@ import { IBooking } from "./bookings.interface"
 
 
 
+// const createBookingInDB = async (
+//   payload: IBooking,
+//   customerId: string
+// ) => {
+//   const {
+//     technicianId,
+//     serviceId,
+//     note,
+//     bookingSlotIds
+//   } = payload;
+
+//   // Check technician exists
+//   const technician = await prisma.technicianProfile.findUnique({
+//     where: {
+//       id: technicianId,
+//     },
+//   });
+
+//   if (!technician) {
+//     throw new Error("Technician not found");
+//   }
+
+//   // Check service exists and belongs to technician
+//   const service = await prisma.service.findFirst({
+//     where: {
+//       id: serviceId,
+//       technicianId: technician.id,
+//       isAvailable: true,
+//     },
+//   });
+
+//   if (!service) {
+//     throw new Error("Service not found or unavailable for this technician"
+//     );
+//   }
+
+
+
+//     // Check slots
+//   const slots = await prisma.bookingSlot.findMany({
+//     where: {
+//       id: {
+//         in: bookingSlotIds,
+//       },
+//       serviceId,
+//       isAvailable: true,
+//       isBooked: false,
+//     },
+//   });
+
+//   if (slots.length !== bookingSlotIds.length) {
+//     throw new Error("One or more booking slots are unavailable");
+//   }
+
+//   // Create booking
+//   const booking = await prisma.booking.create({
+//     data: {
+//       customerId,
+//       technicianId,
+//       serviceId,
+//       note,
+//     },
+//     include: {
+//       customer: true,
+//       technician: {
+//         include: {
+//           user: true,
+//         },
+//       },
+//       service: true,
+//     },
+//   });
+
+//   return booking;
+// };
+
+
+
+
+
+
 const createBookingInDB = async (
   payload: IBooking,
   customerId: string
@@ -13,9 +94,10 @@ const createBookingInDB = async (
     technicianId,
     serviceId,
     note,
+    bookingSlotIds,
   } = payload;
 
-  // Check technician exists
+  // Check technician
   const technician = await prisma.technicianProfile.findUnique({
     where: {
       id: technicianId,
@@ -30,44 +112,78 @@ const createBookingInDB = async (
   const service = await prisma.service.findFirst({
     where: {
       id: serviceId,
-      technicianId,
+      technicianId: technician.id,
       isAvailable: true,
     },
   });
 
   if (!service) {
-    throw new Error("Service not found or unavailable for this technician"
-    );
+    throw new Error("Service not found or unavailable for this technician");
   }
 
-  // Create booking
-  const booking = await prisma.booking.create({
-    data: {
-      customerId,
-      technicianId,
+  // Check slots
+  const slots = await prisma.bookingSlot.findMany({
+    where: {
+      id: {
+        in: bookingSlotIds,
+      },
       serviceId,
-      note,
+      isAvailable: true,
+      isBooked: false,
     },
-    include: {
-      customer: true,
-      technician: {
-        include: {
-          user: true,
+  });
+
+  if (slots.length !== bookingSlotIds.length) {
+    throw new Error("One or more booking slots are unavailable");
+  }
+
+  const booking = await prisma.$transaction(async (tx) => {
+    // Create booking
+    const newBooking = await tx.booking.create({
+      data: {
+        customerId,
+        technicianId,
+        serviceId,
+        note,
+      },
+    });
+
+    // Update slots
+    await tx.bookingSlot.updateMany({
+      where: {
+        id: {
+          in: bookingSlotIds,
         },
       },
-      service: true,
-    },
+      data: {
+        bookingId: newBooking.id,
+        isBooked: true,
+        isAvailable: false,
+        bookedCount: {
+          increment: 1,
+        },
+      },
+    });
+
+    return tx.booking.findUnique({
+      where: {
+        id: newBooking.id,
+      },
+      include: {
+        customer: true,
+        technician: {
+          include: {
+            user: true,
+          },
+        },
+        service: true,
+        bookingSlot: true,
+      },
+    });
   });
 
   return booking;
 };
-
-
-
-
-
-
-
 
 
 
